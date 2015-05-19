bf77caa3a4e9c93c3679ff0060bf5777573df2e7
khasathan <khasathan@gmail.com>
2015-05-19 15:20:23 +0700

    remove invalid variables

--- emailsolver.py
+++ emailsolver.py
@@ -15,9 +15,9 @@ def main():
     is_ = solver.letters_to_dec('is')
     ijlegcod_ = solver.letters_to_dec('ijlegcod')
     a_ = solver.letters_to_dec('a')
-    #_game = solver.letters_to_dec('game')
-    #_of = solver.letters_to_dec('of')
-    #_love = solver.letters_to_dec('love')
+    #game_ = solver.letters_to_dec('game')
+    #of_ = solver.letters_to_dec('of')
+    #love_ = solver.letters_to_dec('love')
     b_ = solver.letters_to_dec('b')
     zanroo_ = solver.letters_to_dec('zanroo')
     tmfafj_ = solver.letters_to_dec('tmfafj')
@@ -26,7 +26,7 @@ def main():
     # [([dota + is + ijlegcod] / [a * (game ^of ^ love) + b]) * zanroo] + tmfafj
     # We can ignore part (game^of^love) of expression
     # because it multiply by 'a' and 'a' = 0
-    answer_dec = int((((dota_ + is_ + ijlegcod_) / (a_ * (0) + b_)) * zanroo_) + tmfafj_)
+    answer_dec = int((((dota_ + is_ + ijlegcod_) / (0 + b_)) * zanroo_) + tmfafj_)
     answer_letters = solver.dec_to_letters(answer_dec)
 
     print('Answer: ', end='')
